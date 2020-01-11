081c49e769d7dd2e02de98c9a1b6538d024ade1d
apocal <beqa91@gmail.com>
2020-01-11 04:15:54 +0400

    Add project files.

---
+++ MartiviApiCore/Controllers/MenusController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using MartiviApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MartiviApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MenusController : ControllerBase
+    {
+        MartiviDbContext martiviDbContext;
+
+        public MenusController(MartiviDbContext db)
+        {
+            martiviDbContext = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetMenus()
+        {
+            var menus = martiviDbContext.Menus.Include("SubMenus");
+            return Ok(menus);
+        }
+        
+        [Route("id/{id}")]
+        [HttpGet]
+        public IActionResult GetMenu(int id)
+        {
+            var menu = martiviDbContext.Menus.Include("SubMenus").FirstOrDefault(m=>m.Id==id);
+            if (menu==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(menu);
+        }
+    }
+}
---
+++ MartiviApiCore/Data/MartiviDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MartiviApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MartiviApi.Data
+{
+    public class MartiviDbContext : DbContext
+    {
+        public MartiviDbContext(DbContextOptions<MartiviDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
